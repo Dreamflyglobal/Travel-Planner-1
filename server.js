0bc7fe125d4c74646a6e3ab1b0733b64c09b8256
Dreamflyglobal <dreamflyglobal@gmail.com>
2026-04-20 12:16:39 +0000

    final fix

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// serve from root dist
-app.use(express.static(path.join(process.cwd(), "dist/public")));
+// fix for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("SERVER STARTED");
+
+// correct absolute path
+const publicPath = path.join(__dirname, "dist/public");
+
+app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "dist/public/index.html"));
+  console.log("REQUEST:", req.url);
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 app.listen(PORT, () => {
